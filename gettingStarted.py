b3fc1fa9153852315d90ba6787cf2ccc28c9ed90
Joe <wjs5337@nyu.edu>
2026-05-29 17:40:46 -0400

    Testing minor code change.

--- gettingStarted.py
+++ gettingStarted.py
@@ -6,7 +6,7 @@ def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
     if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "The student should type the answer here"
+        answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "The student should type the answer here"
     else: 
